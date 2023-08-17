d15c6849d00c7ab2e5a242bcaa46df52eaf863fe
safarlyss <sitarsfrli@gmail.com>
2023-08-17 15:29:41 +0400

    img-zoom

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var options1 = {
+    width: 400,
+    zoomWidth: 500,
+    offset: {vertical: 0, horizontal: 10}
+};
+var options2 = {
+    fillContainer: true,
+    offset: {vertical: 0, horizontal: 10}
+};
+new ImageZoom(document.getElementById("img-zoom"), options2);
+var options5 = {
+    width: 400,
+    zoomWidth: 500,
+    offset: {vertical: 0, horizontal: 10}
+};
+var options6 = {
+    fillContainer: true,
+    offset: {vertical: 0, horizontal: 10}
+};
+new ImageZoom(document.getElementById("img-zoommm"), options6);
